b09f8836dde8596b7d0913edc6b85460c74ccef4
ALLION Benjamin <benjamin.allion.pro@gmail.com>
2020-07-02 15:41:04 +0200

    fix(elastic): convert all methods to async.

--- ElasticService.js
+++ ElasticService.js
@@ -1,43 +1,63 @@
-const elasticsearch = require('elasticsearch');
-const REQUEST_TIMEOUT = 30000 // 30s
+const { Client } = require('@elastic/elasticsearch');
+const DEFAULT_PAGE_SIZE = 100;
+const DEFAULT_INDEX = 'default_index_2'
+
+const RequestTypes = {
+    MATCH_REQUEST: 0
+}
 
 class ElasticService {
 
     constructor(hosts) {
-        this.client = new elasticsearch.Client({ hosts });
-        this.pingServer()
+        this.client = new Client({node: hosts});
+    }
+
+    /**
+     * Ping & Init ES Server
+     * @returns {Promise<void>}
+     */
+    async init() {
+        await this.pingServer();
+        await this.createDefaultIndex();
     }
 
     /**
      * Method that ping ES Server and return if connection is ok or not
      */
-    pingServer() {
-        this.client.ping({
-            requestTimeout: REQUEST_TIMEOUT,
-        }, function (error) {
-            if (error) {
-                throw new Error('Elasticsearch cluster is down!')
-            } else {
-                console.log('Everything is ok');
-                return true;
-            }
-        });
+    async pingServer() {
+        try {
+            await this.client.ping();
+            console.log('Elasticsearch connection established.');
+        } catch (exception) {
+            throw new Error('Elasticsearch cluster is down!')
+        }
+    }
+
+    /**
+     * Try to create default index, do nothing if already exists
+     */
+    async createDefaultIndex(){
+        try {
+            await this.createIndex()
+        } catch (error){
+            console.log(error)
+            // Do Nothing
+        }
     }
 
     /**
      * Try to create a new ES index
      * @param indexName
      */
-    createIndex(indexName) {
-        this.client.indices.create({
-            index: indexName
-        }, function (error, response, status) {
-            if (error) {
-                throw new Error(`[ElasticService][createIndex] Index creation failed : ${error}`)
-            } else {
-                return response;
-            }
-        });
+    async createIndex(indexName = DEFAULT_INDEX) {
+        try {
+          await this.client.indices.create({
+              index: indexName
+          });
+          console.log(`[ElasticService][createIndex] '${indexName}' index creation done.`);
+        } catch (error){
+            throw new Error(`[ElasticService][createIndex] Index creation failed : ${error}`);
+        }
     }
 
     /**
@@ -47,18 +67,83 @@ class ElasticService {
      * @param {string} type
      * @param {object} data
      */
-    addDocumentToIndex(indexName, id, type, data){
+    addDocumentToIndex(
+        {
+            indexName = DEFAULT_INDEX,
+            id,
+            data
+        }) {
         this.client.index({
             index: indexName,
             id,
-            type,
             body: data
-        }, function(error, response, status) {
+        }, function (error, response, status) {
             if (error) {
-                throw new Error(`[ElasticService][createIndex] Document injection failed : ${err}`)
+                throw new Error(`[ElasticService][createIndex] Document injection failed : ${error}`)
             } else {
                 return response;
             }
         });
     }
+
+    /**
+     * Do a search based on request options.
+     * @param {string} indexName
+     * @param {number} requestType
+     * @param {object} fields
+     * @param {number} start
+     * @param {number} end
+     */
+    async search(
+        {
+            indexName = DEFAULT_INDEX,
+            requestType = RequestTypes.MATCH_REQUEST,
+            fields = {},
+            pagination= {
+                start: 0,
+                end: DEFAULT_PAGE_SIZE
+            }
+        }) {
+        const request = this.getRequest(arguments[0]);
+        const results = await this.client.search({
+            index: indexName,
+            body: request
+        })
+        return results.body.hits.hits;
+    }
+
+    /**
+     * Get ElasticSearch Request from request options.
+     * @param {number} requestType
+     * @param {object} fields
+     * @param {number} start
+     * @param {number} end
+     * @returns {object}
+     */
+    getRequest(
+        {
+            requestType = RequestTypes.MATCH_REQUEST,
+            fields = {},
+            pagination = {
+                start: 0,
+                end: DEFAULT_PAGE_SIZE
+            },
+        }) {
+        switch (requestType) {
+            default: {
+                return {
+                    from: pagination.start,
+                    size: pagination.end,
+                    query: {
+                        match: fields
+                    }
+                }
+            }
+        }
+    }
+}
+
+module.exports = {
+    ElasticService,
+    RequestTypes
 }
